cd177335d7a4347c820a66284b169c95e6e2dc09
alpharaoh <akaamplaysmc@gmail.com>
2020-09-13 16:38:02 +0100

    Update start.py

--- start.py
+++ start.py
@@ -9,26 +9,15 @@ os.system("cls")
 print("\n[*] Warming up engine...\n\n")
 print("[*] A chrome window will open now...\n[*] Enter your discord credentials in the window before we start")
 from modules import module_grabscreen
+from modules.config import *
 
-from threading import Thread
-
-class start_engine:
-    def __init__(self, xResolution: int, yResolution:int):
-        self.x, self.y = xResolution, yResolution
-        self.run_main()
-
-    def grabEndingScreen(self):
-        module_grabscreen.grabEndingScreen(self.x, self.y)
-
-    def grabDiscussionTitle(self):
-        module_grabscreen.grabDiscussionTitle(self.x, self.y)
-
-    def run_main(self):
-        thread2 = Thread(target = self.grabEndingScreen) #need to grab 2 screens at a time
-        thread3 = Thread(target = self.grabDiscussionTitle) 
-        
-        thread2.start()
-        thread3.start()
+def grabScreen(self):
+    module_grabscreen.grabScreen(self.x, self.y)
 
 if __name__ == "__main__":
-    start = start_engine(1280, 720) #Please leave these values alone
+    try:
+        x, y = int(screen_resolution.split("x")[0]), int(screen_resolution.split("x")[1])
+    except Exception as e:
+        print(f'{e}\n\nSet a valid resolution: e.g. "1366x768" (keeping the quotation marks)')
+
+    module_grabscreen.grabScreen(x,y)
